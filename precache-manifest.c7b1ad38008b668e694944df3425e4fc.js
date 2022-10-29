3e0303adb048e6ec9327eac7d162f6eb16a56e50
Pushtishahh <pushtishahh@gmail.com>
2022-10-29 19:16:12 +0530

    Updates

---
+++ precache-manifest.c7b1ad38008b668e694944df3425e4fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952f64da6cabc63fd7a106757312e17e",
+    "url": "/calorietracker/index.html"
+  },
+  {
+    "revision": "61fd0d8a52d1eadf28a2",
+    "url": "/calorietracker/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "85b540aa43f0b7b21fd3",
+    "url": "/calorietracker/static/js/2.ebfe4a31.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calorietracker/static/js/2.ebfe4a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fd0d8a52d1eadf28a2",
+    "url": "/calorietracker/static/js/main.8c682797.chunk.js"
+  },
+  {
+    "revision": "390f3ebd73e2f258a223",
+    "url": "/calorietracker/static/js/runtime-main.008ac955.js"
+  }
+]);
